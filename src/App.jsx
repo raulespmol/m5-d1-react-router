1c9331d3d78ee53394fa8545f3a710b41cea7fe9
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-12 22:04:24 -0300

    crear rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Contacto from "./pages/Contacto"
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/contacto"
+        element={<Contacto />}
+      />
+      </Routes>
     </>
   )
 }
