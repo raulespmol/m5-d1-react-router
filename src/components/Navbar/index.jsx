08e09851dae4506feb47bfc9aca1b0c9f1a8d1f4
Raul Espinoza M <raul.espmol@gmail.com>
2024-02-12 22:00:07 -0300

    crear navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import { Container, Nav, Navbar as BootstrapNavbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import './style.css'
+
+const Navbar = () => {
+  return (
+    <BootstrapNavbar bg="dark" variant="dark">
+      <Container>
+        <Nav>
+          <Link to="/">Inicio</Link>
+          <Link to="/contacto">Contacto</Link>
+        </Nav>
+        <Nav>
+          <BootstrapNavbar.Brand>Happy Cake</BootstrapNavbar.Brand>
+        </Nav>
+      </Container>
+    </BootstrapNavbar>
+  )
+}
+
+export default Navbar
